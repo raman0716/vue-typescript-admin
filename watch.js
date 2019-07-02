8442770ec9d62ea31a7786b37146e122c8668d96
raman <ramanliu@foxmail.com>
2019-07-02 11:07:35 +0800

    init:fix watch

--- watch.js
+++ watch.js
@@ -7,6 +7,8 @@ const pageRoot = path.resolve(__dirname, './src/pages');
 // eslint-disable-next-line
 watch.watchTree(pageRoot, function (file, curr, prev) {
   if(prev === null ||(curr && curr.size === 0)) {
+    if(Object.values(file).join('').indexOf('.vue') > 0) {
     shell.exec("node ./route-generator.js");
+    }
   }
 });
